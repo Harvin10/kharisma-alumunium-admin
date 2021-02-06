861eb733365fd0e4b43e7dd2be247ea19c9daa18
harvin pratama putra <harvin.pratama@gmail.com>
2021-02-06 15:50:14 +0700

    header done, unitOut input item for frontend done

---
+++ data.php
@@ -0,0 +1,29 @@
+<?php
+// $conn = new mysqli("sql105.epizy.com", "epiz_27719916", "JLlFVEoRB5", "epiz_27719916_shop");
+date_default_timezone_set('Asia/Jakarta');
+$conn = new mysqli("localhost", "root", "", "kharisma");
+
+function read($sql)
+{
+    global $conn;
+    $item = $conn->query($sql);
+    $database = [];
+    while ($data = $item->fetch_row()) {
+        $database[] = $data;
+    }
+
+    return $database;
+}
+
+function write($sql)
+{
+    global $conn;
+    $conn->query($sql);
+    return true;
+}
+
+function rupiah($angka)
+{
+    $hasil_rupiah = "Rp " . number_format($angka, 2, ',', '.');
+    return $hasil_rupiah;
+}
---
+++ rawOut.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <section class="header">
+        <div class="logo">
+            <a href="unitOut.php"><img src="#" alt="Logo"></a>
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="rawIn.php">Barang Datang</a></li> <!-- rawIn -->
+                <li><a href="rawOut.php">Barang Dipakai</a></li> <!-- rawOut -->
+                <li><a href="unitOut.php">Barang Dijual</a></li> <!-- unitOut -->
+            </ul>
+        </div>
+        <div class="profile">
+            <a href="#"></a> <!-- profile -->
+        </div>
+    </section>
+    <section class="main">
+
+    </section>
+</body>
+
+</html>
